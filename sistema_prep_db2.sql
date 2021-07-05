51bb589e548fd75552a97663f9ddc6c4e8fe1730
aaronvegu <aaron@reckoner-rde.io>
2021-07-05 11:14:51 -0500

    SQL script to generate the database for the project

---
+++ sistema_prep_db2.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sistema_votos
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `candidaturas`
+--
+
+DROP TABLE IF EXISTS `candidaturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `candidaturas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_cand` varchar(50) NOT NULL,
+  `partido` varchar(50) NOT NULL,
+  `local` tinyint(1) NOT NULL,
+  `federal` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `candidaturas`
+--
+
+LOCK TABLES `candidaturas` WRITE;
+/*!40000 ALTER TABLE `candidaturas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `candidaturas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `casillas`
+--
+
+DROP TABLE IF EXISTS `casillas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `casillas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_casilla` varchar(30) NOT NULL,
+  `municipio` varchar(50) NOT NULL,
+  `sección` int(11) NOT NULL,
+  `distrito` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `casillas`
+--
+
+LOCK TABLES `casillas` WRITE;
+/*!40000 ALTER TABLE `casillas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `casillas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `apellidos` varchar(60) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `calle` varchar(60) NOT NULL,
+  `colonia` varchar(50) NOT NULL,
+  `num_int` int(15) NOT NULL,
+  `num_ext` int(15) NOT NULL,
+  `municipio` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `codigo_postal` varchar(50) NOT NULL,
+  `aprobado` tinyint(1) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `votos`
+--
+
+DROP TABLE IF EXISTS `votos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `votos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `casilla` int(11) NOT NULL,
+  `candidatura` int(11) NOT NULL,
+  `cantidad` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `casilla` (`casilla`),
+  KEY `candidatura` (`candidatura`),
+  CONSTRAINT `votos_ibfk_1` FOREIGN KEY (`casilla`) REFERENCES `casillas` (`id`),
+  CONSTRAINT `votos_ibfk_3` FOREIGN KEY (`candidatura`) REFERENCES `candidaturas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `votos`
+--
+
+LOCK TABLES `votos` WRITE;
+/*!40000 ALTER TABLE `votos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `votos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-21 10:12:07
